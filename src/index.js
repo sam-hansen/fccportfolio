1ba582e915f42ee5f87115040a2b32e5b2283ce5
Sam Hansen <smhansen07@gmail.com>
2018-12-25 21:08:46 -0700

    setup

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Thanks for the visit!</h1>
+      <h2>CURRENTLY UNDER CONSTRUCTION WHILE IN DEVELOPMENT</h2>
     </div>
   );
 }
