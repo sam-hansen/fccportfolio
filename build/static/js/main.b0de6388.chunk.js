0ff8b9bd6fd6e2137008011a9397298dde959fad
Sam Hansen <20589181+sam-hansen@users.noreply.github.com>
2021-07-07 19:26:36 -0600

    build

---
+++ build/static/js/main.b0de6388.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmyportfolio=this.webpackJsonpmyportfolio||[]).push([[0],{11:function(t,e,n){},4:function(t,e,n){"use strict";n.r(e);n(2);var s=n(3),c=n.n(s),o=(n(9),n(11),n(0));function r(){return Object(o.jsxs)("div",{className:"App",children:[Object(o.jsx)("h1",{children:"Thanks for the visit!"}),Object(o.jsx)("h2",{children:"CURRENTLY UNDER CONSTRUCTION WHILE IN DEVELOPMENT"})]})}var i=document.getElementById("root");c.a.render(Object(o.jsx)(r,{}),i)}},[[4,1,2]]]);
+//# sourceMappingURL=main.b0de6388.chunk.js.map
